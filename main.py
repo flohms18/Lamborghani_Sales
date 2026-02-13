be3a64a492c1a09d22709fc8308dd6f13652aa0c
flohms18 <trouilletflorent@outlook.com>
2026-02-13 15:19:38 +0100

    Added Props

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ print(FT)
 
 fig, ax = plt.subplots()
 fig.set_facecolor('#F8F9FA')
-ax.pie(FT,colors=colors_array,labels=labels_array)
+ax.pie(FT,colors=colors_array,labels=labels_array,autopct='%1.1f%%',textprops= {
+'color' : '#F8F9FA'
+})
 plt.title("Fuel Type of Lamborghini cars sold from 2020-2025")
 plt.show()
